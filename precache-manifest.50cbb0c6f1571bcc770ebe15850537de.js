8cad76628afc731028a10c84870d8fc3e14bfe14
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-01 02:42:24 +0300

    Updates

---
+++ precache-manifest.50cbb0c6f1571bcc770ebe15850537de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ce267084fa894962431a07cdbb47d0",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "8f45e123cf44d55a1eca",
+    "url": "/todo-list/static/css/main.88d9ecd5.chunk.css"
+  },
+  {
+    "revision": "a6d384e0b3b4880a89ab",
+    "url": "/todo-list/static/js/2.91da2ce8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.91da2ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f45e123cf44d55a1eca",
+    "url": "/todo-list/static/js/main.c8ef56ab.chunk.js"
+  },
+  {
+    "revision": "8f087560a89d664a01f9",
+    "url": "/todo-list/static/js/runtime-main.9f213b1d.js"
+  },
+  {
+    "revision": "f371ec2b9ef444d7046dd522c6a7de27",
+    "url": "/todo-list/static/media/welcome_page.f371ec2b.png"
+  }
+]);
